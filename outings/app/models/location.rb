061d1d1c0f8ce4661b198918b6d16d390b12ba23
Craig Iturbe <citurbe@gmail.com>
2016-10-29 15:45:11 -0400

    add a few methods to location model

--- outings/app/models/location.rb
+++ outings/app/models/location.rb
@@ -18,10 +18,29 @@ class Location < ApplicationRecord
   validates :name, presence: true
 
   def display_score
+    return 0 if self.reviews == []
     reviews = self.reviews.map {|revs| revs.score}
     num = 0
     reviews.each {|rev| num += rev}
     num.to_f/reviews.size
+  end
+
+  def self.random_loc
+    random_loc = Location.all.sample
+  end
+
+  def self.highest_rated
+    locations = Location.all.sort_by {|loc| loc.display_score}
+    locations.first
+  end
+
+  def number_of_plans
+    self.plans.count
+  end
+
+  def self.most_popular
+    locations = Location.all.sort_by {|loc| loc.number_of_plans}
+    locations.first
   end
 
 end
