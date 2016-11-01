c59475cc0b4aa15059dbff8d8a529f82239b69ad
Craig Iturbe <citurbe@gmail.com>
2016-11-01 14:39:57 -0400

    add google maps support

--- outings/app/controllers/locations_controller.rb
+++ outings/app/controllers/locations_controller.rb
@@ -5,5 +5,22 @@ class LocationsController < ApplicationController
 
     # @review_chart_data = @location.reviews_chart_data
     @column_chart_library = @location.column_chart_library
+    mapper = Mapper.new(@location)
+    @map_string = mapper.run
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    redirect_to location_path(@location)
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :address)
   end
 end
